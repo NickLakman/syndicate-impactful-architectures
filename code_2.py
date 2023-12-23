46e292b69c79671bbf5068e4dc948c753d471ae1
NickLakman <140800149+NickLakman@users.noreply.github.com>
2023-12-23 21:21:41 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,26 @@
+        int[] data = new int[10];
+}
+        for (int i = 0; i < 10; i++) {
+    public static void main(String[] args) {
+            data[i] = (int) (Math.random() * 100) + 1;
+            System.out.println("Random Number: " + item);
+        }
+public class RandomDataGenerator {
+
+    }
+        }
+        for (int item : data) {
+def main():
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+
+    main()
+
+
+if __name__ == "__main__":
+    return data
+    for item in data:
